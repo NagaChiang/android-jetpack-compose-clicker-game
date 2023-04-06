95a2252f9b8fa79feb9054d469dc09de5cc306d7
Naga <nagachiangdev@gmail.com>
2023-04-06 16:06:36 +0800

    Add main screen layout

--- app/src/main/java/com/example/composeclickergame/MainActivity.kt
+++ app/src/main/java/com/example/composeclickergame/MainActivity.kt
@@ -3,41 +3,69 @@ package com.example.composeclickergame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeclickergame.ui.theme.ComposeClickerGameTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupUi()
+    }
+
+    private fun setupUi() {
         setContent {
-            ComposeClickerGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeClickerGameTheme {
-        Greeting("Android")
+fun MainScreen() {
+    ComposeClickerGameTheme(
+        darkTheme = true
+    ) {
+        Scaffold { contentPadding ->
+            Box(modifier = Modifier.padding(contentPadding)) {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(
+                            text = "12345",
+                            style = MaterialTheme.typography.displayMedium,
+                        )
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        FilledIconButton(
+                            modifier = Modifier.size(80.dp),
+                            onClick = {}
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Add,
+                                contentDescription = "Add",
+                                modifier = Modifier.size(64.dp),
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
 }
