4a132fec8fd8fb909b078e4495cc8f4b0969b9a1
Naga <nagachiangdev@gmail.com>
2023-04-07 12:01:36 +0800

    Add Hilt for DI

---
+++ app/src/main/java/com/example/composeclickergame/common/HiltApp.kt
@@ -0,0 +1,7 @@
+package com.example.composeclickergame.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApp : Application()
--- app/src/main/java/com/example/composeclickergame/view/GameViewModel.kt
+++ app/src/main/java/com/example/composeclickergame/view/GameViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.composeclickergame.view
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
-class GameViewModel : ViewModel() {
+@HiltViewModel
+class GameViewModel @Inject constructor() : ViewModel() {
     private val _count = MutableStateFlow(0)
     val count = _count.asStateFlow()
 
